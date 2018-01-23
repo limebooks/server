474aefef4d38bfe2c34dfe15fd6750c33741ec37
warlordlizard <alexjbowers88@gmail.com>
2018-01-23 08:26:32 -0800

    server

--- server.js
+++ server.js
@@ -20,3 +20,51 @@ app.get('', (req, res) => {
 app.post('/books', (req, res) => {
   res.send('you posted')
 });
+app.get('/db/person', function (request, response) {
+  client.query('SELECT * FROM persons;')
+    .then(function (data) {
+      response.send(data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
+
+app.post('/db/person', function (request, response) {
+  client.query(`
+    INSERT INTO persons(name, age, ninja)
+    VALUES($1, $2, $3);
+    `,
+    [
+      request.body.name,
+      request.body.age,
+      request.body.ninja
+    ]
+  )
+    .then(function (data) {
+      response.redirect('/');
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
+
+createTable();
+
+app.listen(PORT, () => {
+  console.log(`currently listening on ${PORT}`);
+});
+
+function createTable() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS persons(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(256),
+      age INTEGER,
+      ninja BOOLEAN
+    );`
+  )
+    .then(function (response) {
+      console.log('created table in db!!!!');
+    });
+};
