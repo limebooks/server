5d01d7581f29b2830804476216419203a9461ae6
warlordlizard <alexjbowers88@gmail.com>
2018-01-28 13:14:40 -0800

    changes

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const cors = require('cors');
+//Requires
 const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Additional consts
 const app = express();
 const PORT = process.env.PORT;
-
 const conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
-client.connect();
 
+//set up cors
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(function (req, res, next) {
   req.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,21 +21,19 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('', (req, res) => {
-  res.send('you got me')
-});
-// app.post('/books', (req, res) => {
-//   res.send('you posted')
-// });
-app.get('/api/v1/books', function (request, response) {
+//connect to db
+client.connect();
+
+//set up body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//GETS
+app.get('/api/v1/books', (req, res) => {
   client.query('SELECT * FROM books;')
-    .then(function (result) {
-      response.send(result.rows);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
+    .then(result => res.send(result.rows))
 });
+
 app.get('/api/v1/books/:id', (req, res) => {
   client.query(
     'SELECT * FROM books WHERE book_id=$1;',
@@ -44,52 +41,21 @@ app.get('/api/v1/books/:id', (req, res) => {
     .then(result => res.send(result.rows))
 });
 
-// app.get('/test', (req, res) => res.send('hello world'));
-
-app.post('/api/v1/books', function (request, response) {
-  client.query(`
-    INSERT INTO books(author, title, isbn, url, description)
-    VALUES($1, $2, $3, $4, $5);
-    `,
-    [
-      request.body.title,
-      request.body.author,
-      request.body.url,
-      request.body.isbn,
-      request.body.description
-    ]
-  )
-    .then(function (data) {
-      response.redirect('/');
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
+//POSTS
+app.post('/api/db', (req, res) => {
+  client.query(
+    'INSERT INTO books (author,title,isbn,imgUrl,description) VALUES($1,$2,$3,$4,$5);',
+    [req.body.author, req.body.title, req.body.isbn, req.body.imgUrl, req.body.description],
+    err => console.error(err)
+  );
 });
 
-createTable();
-
-
-function createTable() {
-  client.query(`
-  CREATE TABLE IF NOT EXISTS books(
-    book_id SERIAL PRIMARY KEY,
-    title VARCHAR(256),
-    author VARCHAR(256),
-    url VARCHAR(256),
-    isbn INTEGER,
-    description VARCHAR(800)
-  );`)
-  .then(function (response) {
-    console.log('created table in db!!!!');
-  });
-}
-
+//PUTS
 app.put('/api/db/:id', (req, res) => {
   console.log('HIT PUT ROUTE');
   client.query(
-    'UPDATE books SET author=$1,title=$2,isbn=$3,url=$4,description=$5 WHERE book_id=$6;',
-    [req.body.author, req.body.title, req.body.isbn, req.body.url, req.body.description, req.params.id],
+    'UPDATE books SET author=$1,title=$2,isbn=$3,imgUrl=$4,description=$5 WHERE book_id=$6;',
+    [req.body.author, req.body.title, req.body.isbn, req.body.imgUrl, req.body.description, req.params.id],
     err => console.error(err)
   );
 });
@@ -103,9 +69,4 @@ app.delete('/api/db/:id', (req, res) => {
   );
 });
 
-
-
-
-app.listen(PORT, () => {
-  console.log(`currently listening on ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
